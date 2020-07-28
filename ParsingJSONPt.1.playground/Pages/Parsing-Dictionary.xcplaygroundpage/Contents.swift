d659417ebd95012cc2b418c37d76b23318885b80
uwagboe <sm.uwagboe@gmail.com>
2020-07-28 12:24:33 -0400

    json data was parsed successfully

--- ParsingJSONPt.1.playground/Pages/Parsing-Dictionary.xcplaygroundpage/Contents.swift
+++ ParsingJSONPt.1.playground/Pages/Parsing-Dictionary.xcplaygroundpage/Contents.swift
@@ -1,1 +1,51 @@
-// Parsing Dictionary 
+// Parsing Dictionary
+import Foundation
+
+let json = """
+{
+ "results": [
+   {
+     "firstName": "John",
+     "lastName": "Appleseed"
+   },
+  {
+    "firstName": "Alex",
+    "lastName": "Paul"
+  }
+ ]
+}
+""".data(using: .utf8)!
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+// create model
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Codable - is an alias that has Codable and Decodable in it
+// Decodable - converts the json data
+// Encodable - converts json data to e.g POST to a Web API
+
+// Top level is a dicitonary
+struct ResultsWrapper: Decodable {
+    let results: [Contact]
+}
+
+struct Contact: Decodable {
+    let firstName: String
+    let lastName: String
+}
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+// decode the JSON to our Swift model
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// it could throw an error
+// you would start with the top level json
+do {
+    let dictionary = try JSONDecoder().decode(ResultsWrapper.self, from: json)
+    let contacts = dictionary.results // [Contacts]
+    dump(contacts)
+} catch {
+    print("this is a decoding error: \(error)")
+}
+
